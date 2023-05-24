3b2a95f0bf0d08c87de6534caff64513bad3b3b5
AkseliManninen <akseli.manninen@gmail.com>
2023-05-24 21:28:29 +0300

    Continuing ex. 1.6.

--- Osa-1/unicafe/src/App.js
+++ Osa-1/unicafe/src/App.js
@@ -6,9 +6,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const header1 = "give feedback"
+  const header2 = "statistics"
+
+  const Header = (props) => (
+    <div>
+      <h1> {props.text} </h1>
+    </div>
+  )
+
   return (
     <div>
-      code here
+      <Header text = {header1}/>
+      <Header text = {header2}/>
     </div>
   )
 }
