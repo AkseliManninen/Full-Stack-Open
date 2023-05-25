d2966d3fb8d33c13baeebe417cc6c699e742c2d4
AkseliManninen <akseli.manninen@gmail.com>
2023-05-25 20:18:51 +0300

    Finalising unicafe

--- Osa-1/unicafe/src/App.js
+++ Osa-1/unicafe/src/App.js
@@ -1,40 +1,52 @@
 import { useState } from 'react'
 
+// iso otsikko
+const Header = (props) => (
+  <div>
+    <h1> {props.text} </h1>
+  </div>
+)
+
+// nappi joka tekee jotain painettaessa
+const Button = (props) => (
+    <button onClick = {props.handleClick}> 
+      {props.text} 
+    </button>
+)
+
+// näyttää tulokset eri kategorioille
+const Results = (props) => (
+  <div>
+    {props.text}
+    {props.score}
+  </div>
+)
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const Header = (props) => (
-    <div>
-      <h1> {props.text} </h1>
-    </div>
-  )
-  
-  // nappi joka tekee jotain painettaessa
-  const Button = (props) => (
-      <button onClick = {props.handleClick}> 
-        {props.text} 
-      </button>
-  )
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
 
-  // 
-  const Results = (props) => (
-    <div>
-      {props.text}
-      {props.score}
-    </div>
-  )
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
       <Header text = "give feedback"/>
-      <Button text = "good" />
-      <Button text = "neutral" />
-      <Button text = "bad" />
+      <Button handleClick = {handleGoodClick} text = "good" />
+      <Button handleClick = {handleNeutralClick} text = "neutral" />
+      <Button handleClick = {handleBadClick} text = "bad" />
       <Header text = "statistics"/>
-
       <Results text = "good " score = {good}/>
       <Results text = "neutral " score = {neutral}/>
       <Results text = "bad " score = {bad}/>
