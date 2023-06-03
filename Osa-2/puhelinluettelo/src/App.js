072831fdb40634af30958df655d58c358ac4b117
AkseliManninen <akseli.manninen@gmail.com>
2023-06-03 18:11:44 +0300

    Finishing 2.8

--- Osa-2/puhelinluettelo/src/App.js
+++ Osa-2/puhelinluettelo/src/App.js
@@ -2,10 +2,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1231244' }
   ]) 
   
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     // estää alkuperäisen arvon lisäämisen
@@ -18,18 +20,24 @@ const App = () => {
       console.log('button clicked', event.target)
       // luo uuden person-objektin
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       // lisää person objectin persons-listalle
       setPersons(persons.concat(personObject))
       setNewName("")
+      setNewNumber("")
     }
   }
-
 
   const handlePersonChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -41,16 +49,19 @@ const App = () => {
           value={newName}
           onChange={handlePersonChange}/>
         </div>
+        <div>number: <input
+          value={newNumber}
+          onChange={handleNumberChange}/>
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <p> {persons.map(person => <p> {person.name} </p>)} </p>
+      <p> {persons.map(person => <p> {person.name} {person.number} </p>)} </p>
     </div>
   )
 
 }
 
 export default App
-
