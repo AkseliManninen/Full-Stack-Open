3a73497f1340de322c3cff47e625f1b0bb19184d
AkseliManninen <akseli.manninen@gmail.com>
2023-06-03 17:37:53 +0300

    Finalizing 2.6

--- Osa-2/puhelinluettelo/src/App.js
+++ Osa-2/puhelinluettelo/src/App.js
@@ -4,26 +4,39 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
+  
   const [newName, setNewName] = useState('')
 
-  const addNumber = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName("")
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit = {addNumber}>
+      <form onSubmit = {addPerson}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName}
+          onChange={handlePersonChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <p> {persons.map(person => person.name)} </p>
+      <h2>Numbersz</h2>
+      <p> {persons.map(person => <p> {person.name} </p>)} </p>
     </div>
   )
 
