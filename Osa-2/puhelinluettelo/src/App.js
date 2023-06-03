4eb493f8c6c6c5b6d9dd8e7a29331f28ce3a2197
AkseliManninen <akseli.manninen@gmail.com>
2023-06-03 19:00:43 +0300

    Finishing 2.9

--- Osa-2/puhelinluettelo/src/App.js
+++ Osa-2/puhelinluettelo/src/App.js
@@ -2,12 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setNewFilterName] = useState('')
+  const [show, setShow] = useState(persons)
 
   const addPerson = (event) => {
     // estää alkuperäisen arvon lisäämisen
@@ -40,9 +44,25 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  // filteröi nimiä syötteen mukaan
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    const newFilterName = event.target.value
+    setNewFilterName(newFilterName)
+    setShow(persons.filter(person => person.name.toLowerCase().includes(newFilterName.toLocaleLowerCase())))
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input
+          value={filterName}
+          onChange={handleFilterChange}/>
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit = {addPerson}>
         <div>
           name: <input 
@@ -58,10 +78,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <p> {persons.map(person => <p> {person.name} {person.number} </p>)} </p>
+      <p> {show.map(person => <p> {person.name} {person.number} </p>)} </p>
     </div>
   )
-
 }
 
 export default App
