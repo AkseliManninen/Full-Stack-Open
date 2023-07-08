b285d23918dde7e3c5615c51d2bec6b4ee488842
AkseliManninen <akseli.manninen@gmail.com>
2023-07-08 15:06:50 +0300

    Continuing 2.18

--- Osa-2/maiden_tiedot/src/App.js
+++ Osa-2/maiden_tiedot/src/App.js
@@ -1,3 +1,4 @@
+
 // komponentti: lomake jolla etsitään maita
 const CountryForm = () => (
   <div>
@@ -5,12 +6,34 @@ const CountryForm = () => (
         find countries <input type="text" />
     </form>
   </div>
-  )
+  );
+
+
+// komponentti: renderöi maat
+const Countries = (props) => { 
+  if (props.countries >= 10) {
+    return <p>Too many matches, speficy another filter</p>
+  }
+
+  else if (props.countries < 10 && props.countries > 1) {
+    return <p>Showing countries list</p>
+  }
+
+  else if (props.countries === 1) {
+    return <p>Showing country's information</p>
+  }
+
+  else {
+    return <p>No countries match the search</p>
+  }
+
+};
 
 const App = () => {
   return (
     <div>
       <CountryForm/>
+      <Countries countries = {1}/>
     </div>
   );
 }
