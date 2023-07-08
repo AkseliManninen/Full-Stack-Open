6635936485b354f23196bb96912904fb2e6b9883
AkseliManninen <akseli.manninen@gmail.com>
2023-07-08 17:47:05 +0300

    Finalizing 2.20

--- Osa-2/maiden_tiedot/src/services/countries.js
+++ Osa-2/maiden_tiedot/src/services/countries.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
 
-// tapahtumaläsittelijä: hakee yhteytiedot palvelimelta
+// tapahtumaläsittelijä: hakee maatiedot palvelimelta
 const getAll = () => {
   return axios.get(baseUrl)
 }
---
+++ Osa-2/maiden_tiedot/src/services/weather.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const apiKey = process.env.REACT_APP_API_KEY
+
+// tapahtumaläsittelijä: hakee säätiedot sääapista
+const getWeather = (capital) => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}`
+    return axios.get(apiUrl)
+}
+
+export default { 
+    getWeather: getWeather, 
+  }
+
