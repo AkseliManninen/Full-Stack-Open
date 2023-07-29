f043dfe711fa88c4e689a41e68d430031b75ae8a
AkseliManninen <akseli.manninen@gmail.com>
2023-07-29 12:28:52 +0300

    Fixing 3.11

--- Osa-3/puhelinluettelo-backend/services/persons.js
+++ Osa-3/puhelinluettelo-backend/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://render-test-uplh.onrender.com/api/persons'
+const baseUrl = 'api/persons'
 
 // tapahtumaläsittelijä: hakee yhteytiedot palvelimelta
 const getAll = () => {
