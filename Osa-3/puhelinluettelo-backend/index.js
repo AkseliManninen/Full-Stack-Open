9a498613d82349ab8748ab1c82bcfe8a2bdce8cd
AkseliManninen <akseli.manninen@gmail.com>
2023-07-29 19:37:57 +0300

    Finishing 3.15

--- Osa-3/puhelinluettelo-backend/index.js
+++ Osa-3/puhelinluettelo-backend/index.js
@@ -42,11 +42,12 @@ res.send(info)
 })
 
 // poistaa id:llä olevan puhelintiedon 
-app.delete('/api/persons/:id', (req, res) => {
-const id = Number(req.params.id)
-persons = persons.filter(person => person.id !== id)
-
-res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+  .then(result => {
+    res.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 // lisää puhelintiedon
