12ed5095bb105b0d331fa77326e9ed7cf2a45f14
AkseliManninen <akseli.manninen@gmail.com>
2023-08-12 14:05:31 +0300

    Continuing with 4.2

--- Osa-4/blogilista/controllers/blogs.js
+++ Osa-4/blogilista/controllers/blogs.js
@@ -1,37 +1,20 @@
 const blogsRouter = require('express').Router()
-const Blog = require('../models/note')
+const Blog = require('../mongo')
 
 blogsRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
   })
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
     })
-    .catch(error => next(error))
-})
-
-notesRouter.post('/', (request, response, next) => {
-  const body = request.body
-
-  const note = new Blog({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
 })
 
 module.exports = blogsRouter
