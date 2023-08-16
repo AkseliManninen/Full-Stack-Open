3694a465b097e3818efaa3be8f4814a943b207ff
AkseliManninen <akseli.manninen@gmail.com>
2023-08-16 09:07:10 +0300

    Refactoring the code to match the expected structure

--- Osa-4/blogilista/models/blog.js
+++ Osa-4/blogilista/models/blog.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose')
-const config = require('./utils/config')
+const config = require('../utils/config')
 
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+  }
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
