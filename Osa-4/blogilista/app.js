864ca375d236e5015f9890f8518b270c366de156
AkseliManninen <akseli.manninen@gmail.com>
2023-08-28 08:14:37 +0300

    Starting 4.18

--- Osa-4/blogilista/app.js
+++ Osa-4/blogilista/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -14,5 +15,7 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 
 app.use('/api/users', usersRouter)
+
+app.use('/api/login', loginRouter)
 
 module.exports = app
