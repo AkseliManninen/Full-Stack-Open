4bafbfe78d082e25daed054478945ea3d4a2373d
AkseliManninen <akseli.manninen@gmail.com>
2023-11-07 11:07:16 +0200

    Finishing 5.5 (Lomake pois näkyvistä)

--- Osa-5/bloglist-frontend/src/components/BlogForm.jsx
+++ Osa-5/bloglist-frontend/src/components/BlogForm.jsx
@@ -4,6 +4,7 @@ const BlogForm = ({ createBlog }) => {
   const [newBlogTitle, setNewBlogTitle] = useState('')
   const [newBlogAuthor, setNewBlogAuthor] = useState('')
   const [newBlogURL, setNewBlogURL] = useState('')
+  const [blogFormVisible, setLoginVisible] = useState(false)
 
   const addBlog = async (event) => {
     console.log("Trying to add a blog")
@@ -16,21 +17,32 @@ const BlogForm = ({ createBlog }) => {
     createBlog(newBlog)
   } 
 
-  return (
+  const hideWhenVisible = { display: blogFormVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogFormVisible ? '' : 'none' }
+  
+    return (
     <div>
-      <h2>create new</h2>
-      <form onSubmit={addBlog}>
-          <div>
-          title: <input value={newBlogTitle} onChange={event => setNewBlogTitle(event.target.value)} />
-          </div>
-          <div>
-          author: <input value={newBlogAuthor} onChange={event => setNewBlogAuthor(event.target.value)} />
-          </div>
-          <div>
-          url: <input value={newBlogURL} onChange={event => setNewBlogURL(event.target.value)} />
-          </div>
-          <button type="submit">create</button>
-      </form>
+      <div style={hideWhenVisible}>
+          <button onClick={() => setLoginVisible(true)}>new note</button>
+      </div>
+      <div style={showWhenVisible}>
+        <h2>create new</h2>
+        <form onSubmit={addBlog}>
+            <div>
+              title: <input value={newBlogTitle} onChange={event => setNewBlogTitle(event.target.value)} />
+            </div>
+            <div>
+              author: <input value={newBlogAuthor} onChange={event => setNewBlogAuthor(event.target.value)} />
+            </div>
+            <div>
+              url: <input value={newBlogURL} onChange={event => setNewBlogURL(event.target.value)} />
+            </div>
+              <button type="submit">create</button>
+            <div>
+              <button type="button" onClick={() => setLoginVisible(false)}>cancel</button>
+            </div>
+        </form>
+      </div>
     </div>
     )
   }
