b307a1203d4a8d9d6520d4872ca964c740d24751
AkseliManninen <akseli.manninen@gmail.com>
2023-11-07 11:28:50 +0200

    Finishing 5.7

--- Osa-5/bloglist-frontend/src/components/Blog.jsx
+++ Osa-5/bloglist-frontend/src/components/Blog.jsx
@@ -3,9 +3,33 @@ import { useState } from 'react'
 const Blog = ({ blog }) => {
   const [blogInfoVisible, setBlogInfoVisible] = useState(false)
 
+  const hideWhenVisible = { display: blogInfoVisible ? 'none' : '' }
+
+  const showWhenVisible = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    display: blogInfoVisible ? '' : 'none' 
+  }
+
   return(
-  <div>
-    {blog.title} {blog.author}
+    <div>
+      <div style={hideWhenVisible}>
+        {blog.title}
+        <button type="button" onClick={() => setBlogInfoVisible(true)}>view</button>
+      </div> 
+      <div style={showWhenVisible}>
+        {blog.title} 
+        <button type="button" onClick={() => setBlogInfoVisible(false)}>hide</button>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}
+          <button type="button">like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
   </div>  
   )
 }
