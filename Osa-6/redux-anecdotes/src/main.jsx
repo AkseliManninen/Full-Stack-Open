28e9961ce2f06eaac3520dce2b83ebaad16390a3
AkseliManninen <akseli.manninen@gmail.com>
2024-01-27 18:35:14 +0200

    Finished 6.14

--- Osa-6/redux-anecdotes/src/main.jsx
+++ Osa-6/redux-anecdotes/src/main.jsx
@@ -2,8 +2,8 @@ import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-
-import anecdoteReducer from './reducers/anecdoteReducer'
+import anecdoteService from '../services/anecdotes'
+import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
 
@@ -14,6 +14,10 @@ const store = configureStore({
     notification: notificationReducer,
   }
 })
+
+anecdoteService.getAll().then(anecdotes =>
+  store.dispatch(setAnecdotes(anecdotes))
+)
 
 console.log(store.getState())
 
